4ed0f98c135429be911737a1878c2b29863991cb
Stephen Hara <sthara216@gmail.com>
2014-08-10 09:10:13 -0400

    procfile and web.js

---
+++ web.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var gzippo = require('gzippo');
+var express = require('express');
+var logger = require('morgan');
+var app = express();
+
+app.use(logger('dev'));
+app.use(gzippo.staticGzip('' + __dirname + '/dist'));
+var server = http.createServer(app);
+server.listen(process.env.PORT || 5000);
