50bbe7dd742af218cc661839133193696e66a5b1
Stephen Hara <sthara216@gmail.com>
2014-08-10 09:15:08 -0400

    update web.js

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var http = require('http');
-var gzippo = require('gzippo');
+//var gzippo = require('gzippo');
 var express = require('express');
-var logger = require('morgan');
+//var logger = require('morgan');
 var app = express();
 
-app.use(logger('dev'));
-app.use(gzippo.staticGzip('' + __dirname + '/dist'));
+//app.use(logger('dev'));
+//app.use(gzippo.staticGzip('' + __dirname + '/dist'));
 var server = http.createServer(app);
 server.listen(process.env.PORT || 5000);
