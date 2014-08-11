0778f949a2c43b08a32385f933a6851e03e93b63
Stephen Hara <sthara216@gmail.com>
2014-08-10 20:06:21 -0400

    maybe working?

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,19 @@
  * Controller of the angularWhyApp
  */
 angular.module('angularWhyApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+
+    var todosInStorage = localStorageService.get('todos');
+
+    $scope.todos = todosInStorage && todosInStorage.split('\n') || [];
+
+    $scope.watch('todos', function () {
+        localStorageService.add('todos', $scope.todos.join('\n'));
+    }, true);
+
     $scope.addTodo = function () {
         $scope.todos.push($scope.item);
-        $scope.todo = '';
+        $scope.item = '';
     };
     $scope.removeTodo = function(index) {
         $scope.todos.splice(index, 1);
